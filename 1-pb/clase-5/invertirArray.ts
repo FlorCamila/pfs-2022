2bae55e0b1c5bcf0f467747917e0a95679e80413
florencia <flor_camilal@hotmail.com>
2022-05-25 12:38:09 -0300

    modifico y agrego ejercicio invertirArray

--- 1-pb/clase-5/invertirArray.ts
+++ 1-pb/clase-5/invertirArray.ts
@@ -2,12 +2,25 @@
 
 import * as rls from 'readline-sync';
 
+// let dimensionArray : number = rls.questionInt("Ingrese la dimension del array: ");
+// let numerosDeseados : number [] = new Array (dimensionArray);
+// let indice : number;
+
+// for (indice = 0; indice < dimensionArray; indice++){
+//     numerosDeseados[indice] = rls.questionInt(`Ingrese el numero que desee : `)
+// }
+
+// console.log (`Array invertido: ${numerosDeseados.reverse()}`)
+
 let dimensionArray : number = rls.questionInt("Ingrese la dimension del array: ");
-let numerosDeseados : string [] = new Array (dimensionArray);
+let numerosDeseados : number [] = new Array (dimensionArray);
+let arrayNumerosDeseadosInvertido : number [] = new Array (dimensionArray);
 let indice : number;
 
-for (indice = 0; indice < dimensionArray; indice++){
-    numerosDeseados[indice] = rls.questionInt(`Ingrese el numero que desee : `)
+for(indice = 0; indice < dimensionArray; indice++){
+    numerosDeseados[indice] = rls.question ("Ingrese el numero que desee: ");
+
+    arrayNumerosDeseadosInvertido[dimensionArray-1-indice] = numerosDeseados[indice];
 }
 
-console.log (numerosDeseados.reverse())
+console.log (`El arreglo elegido por el usuario es: ${numerosDeseados} * Luego lo invertimos y se ve de la siguiente manera: ${arrayNumerosDeseadosInvertido}`);
